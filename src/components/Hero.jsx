4f2b6ac97716a9106e7e927ddfca97afec04ef03
Andrew Sia <andrewsia@Andrews-MacBook-Pro.local>
2024-04-03 21:49:14 -0700

    Initial Commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return (
+        <>
+            <section id="hero">
+                <div>
+                    <h1>Hero</h1>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Hero;
